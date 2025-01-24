801303b81acf1d15e7af948c444cb0a315b98eae
Kasumi Hanazuki <kasumi@rollingapple.net>
2025-01-24 07:08:01 +0000

    example

---
+++ examples/class.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <rcx/rcx.hpp>
+
+void Init_examples_class() {
+  using namespace rcx::arg;
+
+  auto ruby = rcx::Ruby();
+  auto cls =
+      ruby.define_class("Example")
+          .define_method(
+              "initialize",
+              [](rcx::Value self, int foo) -> void { self.instance_variable_set("@foo", foo); },
+              arg<int, "foo">)
+          .define_method("foo",
+              [](rcx::Value self) -> int { return self.instance_variable_get<int>("@foo"); });
+  auto obj = cls.new_instance(42);
+
+  std::cout << "obj.foo=" << obj.send<int>("foo") << std::endl;
+}
